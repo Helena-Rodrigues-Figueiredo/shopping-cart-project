8d6488d8ef5444cf0576bdcfce5c0f0a4c2e43ff
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-05-01 15:20:40 -0300

    Requisito 10

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,12 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se com o argumento "<ol><li>Item</li></ol>" o localStorage.setItem é chamado', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  it('Verifica se com o argumento "<ol><li>Item</li></ol>" o localStorage.setItem é chamado com dois parâmentros "cartItems" e o valor passado para saveCartItems', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', saveCartItems);
+  })
 });
